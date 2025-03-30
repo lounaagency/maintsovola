32aa55adeaaf88ea676fc88d7d0d1459d6da3850
lounaagency <lounaagency619@gmail.com>
2025-03-30 21:19:55 +0300

    Update utils.ts
    intégration de l'ariary

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const formatCurrency = (amount: number, currency: string = "MGA") => {
+  return new Intl.NumberFormat("fr-MG", {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 0, // Ariary n'a pas de sous-unité officielle
+  }).format(amount);
+};
+
+
