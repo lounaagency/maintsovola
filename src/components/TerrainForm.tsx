fe89f244f94eca00c41263667965bd1e74b75992
lounaagency <lounaagency619@gmail.com>
2025-04-01 05:35:14 +0300

    Update TerrainForm.tsx

--- src/components/TerrainForm.tsx
+++ src/components/TerrainForm.tsx
@@ -15,7 +15,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/sélect";
 import { Checkbox } from "@/components/ui/checkbox"
 import { toast } from 'sonner';
 import {
