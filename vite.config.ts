1af2c86f926ddb5d0384184eee68b016f69688ae
louna agency <lounaagency619@gmail.com>
2025-05-12 10:33:35 +0300

    Ajustement de l'index

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/app/", // ← Chemin de base pour un sous-dossier "/app"
   server: {
     host: "::",
     port: 8080,
